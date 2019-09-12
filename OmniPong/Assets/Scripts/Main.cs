e331db64c560def82514678977aae70747df7ccd
zabraf <Administrateur@R104PC11>
2019-09-12 13:38:13 +0200

    Added Resources

---
+++ OmniPong/Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject Paddle1;
+    public GameObject Paddle2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Paddle1.transform.position = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
