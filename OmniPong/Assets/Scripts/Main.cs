c6201e38c9937a5199fa2e6068c741cc0ab75875
zabraf <Administrateur@R104PC11>
2019-09-12 16:10:53 +0200

    Created Template Scene

--- OmniPong/Assets/Scripts/Main.cs
+++ OmniPong/Assets/Scripts/Main.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    public GameObject Paddle1;
-    public GameObject Paddle2;
+    public GameObject PaddleJ1;
+    public GameObject PaddleJ2;
+    public GameObject Ball;
     // Start is called before the first frame update
     void Start()
     {
-        Paddle1.transform.position = 
+        PaddleJ1.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight / 2));
+        PaddleJ1.transform.position = new Vector3(PaddleJ1.transform.position.x + PaddleJ1.GetComponent<SpriteRenderer>().bounds.size.x, PaddleJ1.transform.position.y, 10);
+        PaddleJ2.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight / 2));
+        PaddleJ2.transform.position = new Vector3(PaddleJ2.transform.position.x - PaddleJ2.GetComponent<SpriteRenderer>().bounds.size.x, PaddleJ2.transform.position.y, 10);
+        Ball.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 10));
     }
 
     // Update is called once per frame
